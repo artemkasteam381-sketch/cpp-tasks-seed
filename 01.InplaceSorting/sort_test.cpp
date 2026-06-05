d0058d0a8ca2700fb62f604bea34417aa186d082
artemkasteam381-sketch <artemka.steam.381@gmail.com>
2026-06-05 18:17:40 +0300

    Update sort_test.cpp

--- 01.InplaceSorting/sort_test.cpp
+++ 01.InplaceSorting/sort_test.cpp
@@ -1,28 +1,22 @@
 #include <gtest/gtest.h>
 #include <vector>
 #include <algorithm>
-#include "sorting.h"[cite: 2]
+#include "sorting.h"
 
-// Проверка Bubble Sort
-TEST(SortingTest, BubbleSortCorrectness)
-{
-    std::vector<int> data = {5, 4, 3, 2, 1};
-    bubble_sort(data.begin(), data.end());[cite: 1]
-    EXPECT_TRUE(std::is_sorted(data.begin(), data.end()));[cite: 2]
+TEST(SortingTest, BubbleSortCorrectness) {
+    std::vector<int> data = {5, 3, 8, 4, 1, 2, 9, 7, 6};
+    bubble_sort(data.begin(), data.end());
+    EXPECT_TRUE(std::is_sorted(data.begin(), data.end()));
 }
 
-// Проверка Quick Sort
-TEST(SortingTest, QuickSortCorrectness)
-{
-    std::vector<int> data = {3, 1, 4, 1, 5, 9};
-    quick_sort(data.begin(), data.end());[cite: 1]
-    EXPECT_TRUE(std::is_sorted(data.begin(), data.end()));[cite: 2]
+TEST(SortingTest, QuickSortCorrectness) {
+    std::vector<int> data = {5, 3, 8, 4, 1, 2, 9, 7, 6};
+    quick_sort(data.begin(), data.end());
+    EXPECT_TRUE(std::is_sorted(data.begin(), data.end()));
 }
 
-// Тест на пустой массив (граничный случай)
-TEST(SortingTest, HandlesEmptyRange)
-{
-    std::vector<int> data = {};
-    EXPECT_NO_THROW(bubble_sort(data.begin(), data.end()));[cite: 1]
-    EXPECT_NO_THROW(quick_sort(data.begin(), data.end()));[cite: 1]
+TEST(SortingTest, HandlesEmptyRange) {
+    std::vector<int> data;
+    EXPECT_NO_THROW(bubble_sort(data.begin(), data.end()));
+    EXPECT_NO_THROW(quick_sort(data.begin(), data.end()));
 }
